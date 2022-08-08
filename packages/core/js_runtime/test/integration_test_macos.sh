185310381f8f65d080ebbd94e9094064a3cbfaaa
Longlong Li <lilong7676@outlook.com>
2022-08-08 17:45:03 +0800

    feat(js_runtime): add integration_test_macos support

--- packages/core/js_runtime/test/integration_test_macos.sh
+++ packages/core/js_runtime/test/integration_test_macos.sh
@@ -2,4 +2,4 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 fvm flutter drive --driver=$SCRIPT_DIR/driver/integration_test.dart \
   --target=$SCRIPT_DIR/integration_test/integration_test.dart \
-  -d android
+  -d macos
